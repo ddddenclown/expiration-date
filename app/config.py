57565905194b36dfbcf86b8e7ae9cab968ff84ed
ddddenclown <d19052006d@gmail.com>
2025-08-28 18:45:14 +0300

    changed config

--- app/config.py
+++ app/config.py
@@ -5,8 +5,5 @@ from pathlib import Path
 class Settings(BaseSettings):
     OPENAI_API_KEY: str
 
-    class Config:
-        env_file = env_file = Path(__file__).resolve().parent.parent / ".env"
-
 
 settings = Settings()
--- build_and_run.sh
+++ build_and_run.sh
@@ -9,4 +9,4 @@ docker rm fastapi-faiss || true
 docker build -t fastapi-faiss .
 
 # Запускаем Docker контейнер с меткой ветки и портом 8007
-docker run --label branch=Dima_branch --restart unless-stopped -d -p 8007:8007 -e OPENAI_API_KEY="${OPENAI_API_KEY:-}" --name fastapi-faiss fastapi-faiss
+docker run --label branch=Dima_branch --restart unless-stopped -d -p 8007:8007 -e OPENAI_API_KEY="$OPENAI_API_KEY" --name fastapi-faiss fastapi-faiss
